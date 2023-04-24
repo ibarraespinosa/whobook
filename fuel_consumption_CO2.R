45b49c1d7b8e441a2436fb469deae724a4b81c10
Sergio Ibarra Espinosa <zergioibarra@gmail.com>
2023-04-23 18:41:32 -0600

    add example

--- fuel_consumption_CO2.R
+++ fuel_consumption_CO2.R
@@ -7,7 +7,7 @@ ef1 <- ef_ldv_speed(v = "PC",
                     cc = "<=1400", 
                     f = "G", 
                     eu = "IV",
-                    p = "FC",
+                    p = "CO2",
                     speed = Speed(0:150))
 
 ef2 <- ef_ldv_speed(v = "PC",
@@ -15,7 +15,7 @@ ef2 <- ef_ldv_speed(v = "PC",
                     cc = "1400_2000", 
                     f = "G", 
                     eu = "IV",
-                    p = "FC",
+                    p = "CO2",
                     speed = Speed(0:150))
 
 ef3 <- ef_ldv_speed(v = "PC",
@@ -23,14 +23,46 @@ ef3 <- ef_ldv_speed(v = "PC",
                     cc = ">2000", 
                     f = "G", 
                     eu = "IV",
-                    p = "FC",
+                    p = "CO2",
                     speed = Speed(0:150))
 
 
 df <- data.frame(small = ef1,
                  medium = ef2,
                  big = ef3)
+
 colplot(df, 
-        ylab = "Fuel Consumption g/km", 
+        ylab = expression(CO[2]~g/km), 
         xlab = "km/h", 
         theme = "clean")
+
+png("chapter3_co2_road.png", width = 2000, height = 1500, res = 300)
+colplot(df, 
+        ylab = expression(CO[2]~g/km), 
+        xlab = "km/h", 
+        theme = "clean")
+dev.off()
+
+
+fn <- function(x, label, size = 50) {
+  f <-magick::image_read(x)
+  f <- magick::image_trim(f)
+  f <- magick::image_annotate(f, 
+                              text = label, 
+                              size = size)
+  
+  magick::image_write(image = f, path = x)
+}
+
+fn(x = "chapter3_co2_road.png", label = "")
+
+
+sapply(df, mean)
+
+df$speed <- 0:150
+
+df[df$speed == 33, ] |> unlist() |> round(2)
+
+df[df$small == min(df$small), ] |> unlist() |> round(2)
+df[df$medium == min(df$medium), ] |> unlist() |> round(2)
+df[df$big == min(df$big), ] |> unlist() |> round(2)
